0ba83c6b95b14986a98384f761c0b760aff2cf9c
dustn2524 <kskn0790@gmail.com>
2025-05-08 14:39:26 +0900

    Add files via upload

---
+++ 2025.05.08/src/Main.java
@@ -0,0 +1,33 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("점수를 입력하세요 (0~100): ");
+        int score = scanner.nextInt();
+        String grade;
+
+        switch (score / 10) {
+            case 10:
+            case 9:
+                grade = "A";
+                break;
+            case 8:
+                grade = "B";
+                break;
+            case 7:
+                grade = "C";
+                break;
+            case 6:
+                grade = "D";
+                break;
+            default:
+                grade = "F";
+        }
+
+        System.out.println("학점은 " + grade + "입니다.");
+    }
+}
