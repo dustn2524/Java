da7067184a7961e2655c8b68181f9e00d9fb6aea
dustn2524 <kskn0790@gmail.com>
2025-05-12 14:12:27 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("숫자를 입력하세요: ");
+        int maxNumber = scanner.nextInt();
+
         int sum = 0;
 
-        for (int i = 1; i <= 1000; i++) {
-            if (i % 2 == 0) {
+        for (int i = 1; i <= maxNumber; i++) {
+            if (i % 2 != 0) {
                 sum += i;
             }
         }
 
-        System.out.println("1부터 1000까지 짝수의 합: " + sum);
+        System.out.println("홀수의 합: " + sum);
+
+        scanner.close();
     }
 }
